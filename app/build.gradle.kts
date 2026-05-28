cee1f011693fbb00094b3e904226712c387cc237
Harisunder Chidambaram <harisundereee91@gmail.com>
2026-05-28 17:43:22 +0530

    Corrected Build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:compose-material-icons-extended")
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Room Database
     implementation("androidx.room:room-runtime:2.6.1")
